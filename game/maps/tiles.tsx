83248b119c432d7301fbbf3d17bef08bbdc59495
Boris <b.tseytlin@lambda-it.ru>
2021-01-18 16:37:17 +0300

    Basic walking

--- game/maps/tiles.tsx
+++ game/maps/tiles.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="2021.01.13" name="tiles" tilewidth="128" tileheight="128" tilecount="2" columns="1">
- <image source="tileset.png" width="128" height="256"/>
+ <image source="../assets/tileset.png" width="128" height="256"/>
  <tile id="0">
   <properties>
    <property name="collide" type="bool" value="true"/>
--- game/src/main.js
+++ game/src/main.js
@@ -7,6 +7,9 @@ function objectFromSpawnPoint(game, spawn_point_obj, sprite_key) {
 
 /* Game */
 
+const screenWidth = 800;
+const screenHeight = 600;
+const plySpeed = 500;
 
 let config = {
     type: Phaser.AUTO,
@@ -15,8 +18,8 @@ let config = {
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: {y: 500},
-            debug: false
+            gravity: {y: 1500},
+            debug: true
         }
     },
     scene: {
@@ -32,16 +35,29 @@ let game = new Phaser.Game(config);
 let map;
 let player;
 let cursors;
-let groundLayer, coinLayer;
+let groundLayer;
 let text;
 let score = 0;
 
 
 function preload() {
     this.load.tilemapTiledJSON('map', 'maps/level1.json');
-    this.load.spritesheet('tiles', 'maps/tileset.png', 
+    this.load.spritesheet('tiles', 'assets/tileset.png', 
         {frameWidth: 128, frameHeight: 128});
-    this.load.atlas('player', 'assets/player.png', 'assets/player.json');
+    this.load.spritesheet('player', 'assets/dante.png',
+        {frameWidth: 152, frameHeight: 248});
+    this.load.spritesheet('virgil', 'assets/virgil.png',
+        {frameWidth: 152, frameHeight: 248});
+
+    const level1_assets = [
+        'background', 'star1', 'star2', 'star3', 'strip', 
+        'tree1', 'tree2', 'tree3', 'tree3', 'tree4', 'tree5', 'tree6',
+        'headline'
+    ]
+
+    for (let image_key of level1_assets) {
+        this.load.image(image_key, 'assets/level1/' + image_key + '.png');
+    }
 }
 
 function initMap(game) {
@@ -59,28 +75,54 @@ function initGround(game, map) {
     return ground;
 }
 
-function initBackground(game) {
-    map = game.add.tilemap('map');
-    let groundTiles = map.addTilesetImage('tiles', 'tiles');
-    let ground = map.createLayer('ground', groundTiles);
-    ground.setCollisionByExclusion([-1]);
- 
-    game.physics.world.bounds.width = ground.width;
-    game.physics.world.bounds.height = ground.height;
+function initBackground(game, map) {
 
-    return ground;
+    for (let img of map.imageCollections[0].images) {
+        let parts = img.image.split('/');
+        const fname = parts[parts.length - 1];
+        const key = fname.split('.')[0];
+        map.createFromObjects('background', {gid: img.gid, key: key});
+
+    }
+
+    for (let img of map.imageCollections[0].images) {
+        let parts = img.image.split('/');
+        const fname = parts[parts.length - 1];
+        const key = fname.split('.')[0];
+        map.createFromObjects('background_objects_2', {gid: img.gid, key: key});
+    }
+
+    for (let img of map.imageCollections[0].images) {
+        let parts = img.image.split('/');
+        const fname = parts[parts.length - 1];
+        const key = fname.split('.')[0];
+        map.createFromObjects('background_objects_1', {gid: img.gid, key: key});
+    }
+
+    
 }
 
 
 function initPlayer(game, map) {
-    let player_spawn = map.createFromObjects('game', {name: "player"}, {key: 'player'});
-    let ply_spawn = player_spawn[0];
+    let player_spawns = map.createFromObjects('game', {name: "player"});
+    let ply_spawn = player_spawns[0];
 
     let ply = objectFromSpawnPoint(game, ply_spawn, 'player')
     ply.setBounce(0.1); // our player will bounce from items
     ply.setCollideWorldBounds(true); // don't go out of the map
     return ply;    
 }
+
+function initVirgil(game, map) {
+    let spawns = map.createFromObjects('game', {name: "virgil"});
+    let spawn = spawns[0];
+
+    let obj = objectFromSpawnPoint(game, spawn, 'virgil')
+    obj.setBounce(0.1); // our player will bounce from items
+    obj.setCollideWorldBounds(true); // don't go out of the map
+    return obj;    
+}
+
 
 function initInput(game) {
     return game.input.keyboard.createCursorKeys();
@@ -98,15 +140,37 @@ function initCamera(game) {
 
 function initAnimations(game) { 
     game.anims.create({
+        key: 'idle',
+        frames: game.anims.generateFrameNames('player', 
+            { start: 0, end: 0}),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    game.anims.create({
         key: 'player_walk',
         frames: game.anims.generateFrameNames('player', 
-            { prefix: 'p1_walk', start: 1, end: 11, zeroPad: 2 }),
-        frameRate: 10,
+            { start: 0, end: 4}),
+        frameRate: 5,
+        repeat: -1
+    });
+
+
+    game.anims.create({
+        key: 'virgil_walk',
+        frames: game.anims.generateFrameNames('virgil', 
+            { start: 0, end: 3}),
+        frameRate: 5,
         repeat: -1
     });
 }
 
 function initUI(game) { 
+
+    let headline_img = game.add.sprite(screenWidth/2, 50, 'headline');
+    headline_img.setScrollFactor(0);
+    headline_img.scale = 0.6;
+
     let txt = game.add.text(20, 570, '0', {
         fontSize: '20px',
         fill: '#ffffff'
@@ -117,30 +181,28 @@ function initUI(game) {
 
 function create() {
     map = initMap(this);
+    initBackground(this, map);
     groundLayer = initGround(this, map);  
     player = initPlayer(this, map);
-    
+    virgil = initVirgil(this, map);
     this.physics.add.collider(groundLayer, player);
-
+    this.physics.add.collider(groundLayer, virgil);
     cursors = initInput(this);
-
     initCamera(this);
-
     initAnimations(this);
-
     text = initUI(this);
 }
  
 function update() {
     if (cursors.left.isDown) // if the left arrow key is down
     {
-        player.body.setVelocityX(-200); // move left
+        player.body.setVelocityX(-plySpeed); // move left
         player.anims.play('player_walk', true); // play walk animation
         player.flipX= true; // flip the sprite to the left
     }
     else if (cursors.right.isDown) // if the right arrow key is down
     {
-        player.body.setVelocityX(200); // move right
+        player.body.setVelocityX(plySpeed); // move right
         player.anims.play('player_walk', true); // play walk animatio
         player.flipX = false; // use the original sprite looking to the right
     } else if (player.body.onFloor()) {
